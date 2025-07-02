aa6d987f1f539b6d59a79bdf135566682d95cc92
agritechsales9 <teenwolves9@gmail.com>
2025-07-02 07:06:54 -0700

    add

---
+++ index.js
@@ -0,0 +1,10 @@
+var signupbtn = document.querySelector('#signin');
+var loginbtn = document.querySelector('#login');
+
+signupbtn.addEventListener('click', ()=>{
+    window.location.href = 'sign.html';
+});
+
+loginbtn.addEventListener('click', ()=>{
+    window.location.href = 'login.html';
+});
---
+++ sign.js
@@ -0,0 +1,36 @@
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
+    import { getDatabase, ref, set, get, push, onValue, remove, orderByChild, query, update } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-database.js";
+    import { getStorage, ref as sRef, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-storage.js";
+    import { getStorage, ref as sRef, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
+
+    const firebaseConfig = {
+      apiKey: "AIzaSyC7fCNZe8l-fGhhKsMF0JVcymh8uCv1zyI",
+      authDomain: "lavender-app-eeeb0.firebaseapp.com",
+      databaseURL: "https://lavender-app-eeeb0-default-rtdb.firebaseio.com",
+      projectId: "lavender-app-eeeb0",
+      storageBucket: "lavender-app-eeeb0.appspot.com",
+      messagingSenderId: "666797584961",
+      appId: "1:666797584961:web:8d9576c372d72fe0938cc0",
+      measurementId: "G-ECZ1J6F0KK"
+    };
+
+
+    const app = initializeApp(firebaseConfig);
+    const db = getDatabase(app);
+    const storage = getStorage(app);
+
+
+var usernameHolder = document.querySelector('#username');
+var emailHolder = document.querySelector('#email');
+var passwordHolder = document.querySelector('#passcode');
+var signUpBtn = document.querySelector('#sign-btn');
+
+signUpBtn.addEventListener('click', () => {
+    var username = usernameHolder.value;
+    var email = emailHolder.value;
+    var password = passwordHolder.value;
+    if(username !== '' && email !== '' password !== ''){
+        
+
+    }
+});
