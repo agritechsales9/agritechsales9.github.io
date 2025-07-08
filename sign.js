9b51018648fd9d19c0fcc2fcb95dd7e080c20f5a
agritechsales9 <teenwolves9@gmail.com>
2025-07-08 16:04:11 +0300

    Update sign.js

--- sign.js
+++ sign.js
@@ -1,7 +1,7 @@
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
     import { getDatabase, ref, set, get, push, onValue, remove, orderByChild, query, update } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-database.js";
     import { getStorage, ref as sRef, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-storage.js";
-    import { getStorage, ref as sRef, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
+    import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
 
     const firebaseConfig = {
       apiKey: "AIzaSyC7fCNZe8l-fGhhKsMF0JVcymh8uCv1zyI",
@@ -18,19 +18,37 @@
     const app = initializeApp(firebaseConfig);
     const db = getDatabase(app);
     const storage = getStorage(app);
+    const auth = getAuth(app);
 
 
 var usernameHolder = document.querySelector('#username');
 var emailHolder = document.querySelector('#email');
 var passwordHolder = document.querySelector('#passcode');
 var signUpBtn = document.querySelector('#sign-btn');
+var phonenumber = document.querySelector('#tellno');
 
 signUpBtn.addEventListener('click', () => {
     var username = usernameHolder.value;
     var email = emailHolder.value;
     var password = passwordHolder.value;
-    if(username !== '' && email !== '' password !== ''){
-        
+    var telNo = phonenumber.value;
+    if(username !== '' && email !== '' && password !== '' && telNo !== '') {
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential)=>{
+          var userData = userCredential.user;
+          var userRef = push(ref(db, 'users/'));
+          set(userRef, {
+            username: username,
+            email: email,
+            phonenumber: telNo,
+            uid: userData.uid
+          })
+          .then(()=>{
+            window.location.href = 'main.html';
+          });
+        });
 
+    } else {
+      alert('please fill all the fields.');
     }
 });
